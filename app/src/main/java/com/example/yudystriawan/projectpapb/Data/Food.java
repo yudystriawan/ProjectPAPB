8de6eeff960bccb335cb0518d8ad41fcde261e60
Ade Yudys Triawan <yudistriawan@gmail.com>
2018-12-04 11:09:48 +0700

    added design, spinner

--- app/src/main/java/com/example/yudystriawan/projectpapb/Data/Food.java
+++ app/src/main/java/com/example/yudystriawan/projectpapb/Data/Food.java
@@ -3,12 +3,10 @@ package com.example.yudystriawan.projectpapb.Data;
 public class Food {
     private String desc;
     private int image;
-    private int button;
 
-    public Food(String desc, int image, int button) {
+    public Food(String desc, int image) {
         this.desc = desc;
         this.image = image;
-        this.button = button;
     }
 
     public String getDesc() {
@@ -17,9 +15,5 @@ public class Food {
 
     public int getImage() {
         return image;
-    }
-
-    public int getButton() {
-        return button;
     }
 }
--- app/src/main/java/com/example/yudystriawan/projectpapb/DetailFoodActivity.java
+++ app/src/main/java/com/example/yudystriawan/projectpapb/DetailFoodActivity.java
@@ -103,8 +103,8 @@ public class DetailFoodActivity extends AppCompatActivity {
                         ArrayList<Review> rev = new ArrayList<>();
                         if(sizeData != 0){
                             for (int i = 0; i < sizeData; i++) {
-                                komentar = queryDocumentSnapshots.getDocuments().get(i).get("Username").toString();
-                                nama = queryDocumentSnapshots.getDocuments().get(i).get("Comment").toString();
+                                komentar = queryDocumentSnapshots.getDocuments().get(i).get("Comment").toString();
+                                nama = queryDocumentSnapshots.getDocuments().get(i).get("Username").toString();
                                 rev.add(new Review(nama, komentar));
                             }
                             recyclerRev.setHasFixedSize(true);
